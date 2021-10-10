aa62d9e23206390c0846a1d7ca37edff022e1818
JOJO840 <josip840@gmail.com>
2021-10-10 17:42:12 +0200

    fixade cpu filen

--- cpu_usage.py
+++ cpu_usage.py
@@ -6,19 +6,28 @@ import time
 memory_limit, minutes = sys.argv[1:]
 
 if memory_limit[-1] == 'G':
-    # convert memory_limit from GB to B
-    memory_limit *= 10 ** 9  
-    
-if memory_limit[-1] == 'M':
-    # convert memory_limit from MB to B
-    memory_limit *= 10 ** 6
+    memory_limit = memory_limit.replace("G","")
+    memory_limit = float(memory_limit) 
+    memory_limit *= 10 ** 9     # convert memory_limit from GB to B
+
+
+elif memory_limit[-1] == 'M':
+    memory_limit = memory_limit.replace("M","")
+    memory_limit = float(memory_limit) 
+    memory_limit *= 10 ** 6     # convert memory_limit from MB to B
 
 while True:
-    time.sleep(int(minutes) * 60)
-    print("hej")
-    #used_memory = os.popen('free -t -m').readlines()[-1].split()[2]
-    #if used_memory >= memory_limit:
+    time.sleep(int(minutes) * 2)
+    used_memory = os.popen('free -t').readlines()[-1].split()[2]
+    
+    if float(used_memory) > memory_limit:
+        print("Då")
         
             # create status report
         
 
+  # To Do:
+  
+  # Service filen ska endast kolla RAM tröskeln som angetts, och köra .pyfilen 
+  # om Ram minnet är över tröskeln
+  
