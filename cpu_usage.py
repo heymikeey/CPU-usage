ad797b3f16a7cd40dd4bdf165123e3dfde32715a
JOJO840 <josip840@gmail.com>
2021-10-10 14:37:33 +0200

    ändrade lite i koden om minuterna

--- cpu_usage.py
+++ cpu_usage.py
@@ -2,7 +2,6 @@
 import os
 import sys
 import time
-import winsound
 
 memory_limit, minutes = sys.argv[1:]
 
@@ -15,14 +14,11 @@ if memory_limit[-1] == 'M':
     memory_limit *= 10 ** 6
 
 while True:
-	time.sleep(int(minutes) + 60)
-	used_memory = os.popen('free -t -m').readlines()[-1].split()[2]
-	if used_memory >= memory_limit:
-		#playsound('erro.mp3')
-        # create status report
+    time.sleep(int(minutes) * 60)
+    print("hej")
+    #used_memory = os.popen('free -t -m').readlines()[-1].split()[2]
+    #if used_memory >= memory_limit:
+        
+            # create status report
         
 
-                                                # generate sound
-            frequency = 2500  # Set Frequency To 2500 Hertz
-            duration = 1000  # Set Duration To 1000 ms == 1 second
-            winsound.Beep(frequency, duration)
