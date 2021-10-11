50d0d4d014eefcb99338f1ea7f8f737bb64ca257
heymikeey <micke.j92@gmail.com>
2021-10-11 19:13:57 +0200

    gjorde om if satserna så 2 rader blev 1.

--- cpu_usage.py
+++ cpu_usage.py
@@ -6,14 +6,12 @@ import time
 memory_limit, minutes = sys.argv[1:]
 
 if memory_limit[-1] == 'G':
-    memory_limit = memory_limit.replace("G","")
-    memory_limit = float(memory_limit) 
+    memory_limit = float(memory_limit[:-1])
     memory_limit *= 10 ** 9     # convert memory_limit from GB to B
 
 
 elif memory_limit[-1] == 'M':
-    memory_limit = memory_limit.replace("M","")
-    memory_limit = float(memory_limit) 
+    memory_limit = float(memory_limit[:-1])
     memory_limit *= 10 ** 6     # convert memory_limit from MB to B
 
 while True:
