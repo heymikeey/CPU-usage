656abd0cb4d5cafca7bed4ef1a31dd6c383a104c
heymikeey <micke.j92@gmail.com>
2021-10-12 09:10:01 +0200

    Logfil skrivs till tmp mappen. Skrivs dock inget cpu_usage till den ännu

--- cpu_usage.py
+++ cpu_usage.py
@@ -4,24 +4,25 @@ import sys
 import time
 
 memory_limit, minutes = sys.argv[1:]
+logfile = "/tmp/cpu_usage_rapport.txt"
 
 if memory_limit[-1] == 'G':
     memory_limit = float(memory_limit[:-1])
     memory_limit *= 10 ** 9     # convert memory_limit from GB to B
 
 
-elif memory_limit[-1] == 'M':
+if memory_limit[-1] == 'M':
     memory_limit = float(memory_limit[:-1])
     memory_limit *= 10 ** 6     # convert memory_limit from MB to B
 
 while True:
-    time.sleep(int(minutes) * 2)
+    time.sleep(int(minutes) * 1)
     used_memory = os.popen('free -t').readlines()[-1].split()[2]
     
     if float(used_memory) > memory_limit:
-        print("Då")
+        with open(logfile, 'a') as out:
+             out.write("treshold exceeded" + '\n')  #test to write to location (works) //MJ
         
-            # create status report
         
 
   # To Do:
