635a667204886c1c588012598fa8a0387a0e5fad
heymikeey <micke.j92@gmail.com>
2021-10-13 15:16:41 +0200

    la till funktion som loggar aktiva processer

--- cpu_usage.py
+++ cpu_usage.py
@@ -2,29 +2,51 @@
 import os
 import sys
 import time
+from datetime import datetime
+
+
+def log_processes():
+    processes_raw = os.popen('ps aux').read().splitlines()
+    now = datetime.now()
+    timestamp = now.strftime("%d/%m/%Y %H:%M:%S")
+    log_file = "/tmp/cpu_usage_rapport.txt"
+    log_body = ["*******{}*******".format(timestamp),
+                "[PID]\t[name]\t[memory]"]
+
+    #adds running processes to 'log_body' with pid, name and memory usage in K
+    for p in processes_raw[1:-2]:
+        elements = p.split()
+        pid = elements[1]
+        name = elements[10]
+        log_body.append(pid + "\t" + name + "\t" + os.popen('sudo pmap ' + pid).readlines()[-1].split()[1])
+
+    #writes items in 'log_body' to textfile referenced in 'log_file'
+    with open(log_file, 'a') as out:
+        for item in log_body:
+            out.write("%s\n" % item)
+    return
+
+
 
 memory_limit, minutes = sys.argv[1:]
-logfile = "/tmp/cpu_usage_rapport.txt"
 
 if memory_limit[-1] == 'G':
-    memory_limit = int(memory_limit[:-1])
-    memory_limit *= 1024    # convert memory_limit from GB to MB
+    memory_limit = float(memory_limit[:-1])
+    memory_limit *= 1024                         # convert memory_limit from GB to MB
 
 if memory_limit[-1] == 'M':
     memory_limit = int(memory_limit[:-1])
 
 
-print(memory_limit)                                 #debug
 while True:
-    time.sleep(int(minutes) * 1)
-    used_memory = os.popen('free -t -m').readlines()[-1].split()[2]
+    time.sleep(int(minutes) * 1)                                        #wait x minutes before proceeding
+    used_memory = os.popen('free -t -m').readlines()[-1].split()[2]     #get used memory in MB
     
     if float(used_memory) > memory_limit:
-        print("Writing to log")                     #debug
-        with open(logfile, 'a') as out:
-             out.write("threshold exceeded" + '\n') #test to write to location (works) //MJ
+        log_processes()
         
         
+
 
   # To Do:
   
