1d35cdd134106295a0da28645fa88354e096c107
JOJO840 <josip840@gmail.com>
2021-10-14 14:37:56 +0200

    testade lite

--- cpu_usage.py
+++ cpu_usage.py
@@ -26,30 +26,27 @@ def log_processes():
             out.write("%s\n" % item)
     return
 
+def main():
+
+    memory_limit, minutes = sys.argv[1:]
+
+    if memory_limit[-1] == 'G':
+        memory_limit = float(memory_limit[:-1])
+        memory_limit *= 1024                         # convert memory_limit from GB to MB
+
+    if memory_limit[-1] == 'M':
+        memory_limit = int(memory_limit[:-1])
 
 
-memory_limit, minutes = sys.argv[1:]
+    while True:
+        time.sleep(int(minutes) * 1)                                        #wait x minutes before proceeding
+        used_memory = os.popen('free -t -m').readlines()[-1].split()[2]     #get used memory in MB
 
-if memory_limit[-1] == 'G':
-    memory_limit = float(memory_limit[:-1])
-    memory_limit *= 1024                         # convert memory_limit from GB to MB
+        if float(used_memory) > memory_limit:
+            log_processes()
 
-if memory_limit[-1] == 'M':
-    memory_limit = int(memory_limit[:-1])
-
-
-while True:
-    time.sleep(int(minutes) * 1)                                        #wait x minutes before proceeding
-    used_memory = os.popen('free -t -m').readlines()[-1].split()[2]     #get used memory in MB
-    
-    if float(used_memory) > memory_limit:
-        log_processes()
+if __name__ == "__main__":
+    main()
         
-        
 
 
-  # To Do:
-  
-  # Service filen ska endast kolla RAM tröskeln som angetts, och köra .pyfilen 
-  # om Ram minnet är över tröskeln
-  
