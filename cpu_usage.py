7e96f64fea8d9ca1dbd5b18884fb7105c2bf3b49
heymikeey <micke.j92@gmail.com>
2021-10-14 10:39:34 +0200

    konverterar till minuter istället för sekunder

--- cpu_usage.py
+++ cpu_usage.py
@@ -39,7 +39,7 @@ if memory_limit[-1] == 'M':
 
 
 while True:
-    time.sleep(int(minutes) * 1)                                        #wait x minutes before proceeding
+    time.sleep(int(minutes) * 60)                                        #wait x minutes before proceeding
     used_memory = os.popen('free -t -m').readlines()[-1].split()[2]     #get used memory in MB
     
     if float(used_memory) > memory_limit:
